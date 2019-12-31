770bf731b9523859590f2e19ff0d6c08f278fc02
chentingting <tingtingchen1@jieyuechina.com>
2019-12-31 19:00:17 +0800

    提交

--- shop/src/components/goods/Params.vue
+++ shop/src/components/goods/Params.vue
@@ -4,18 +4,42 @@
       <el-row>
         <el-col>
           <span>选择商品分类:</span>
-          <el-cascader :options="parentCateList" expand-trigger="hover" :props="cascaderProps" @change="handleChange" clearable></el-cascader>
+          <el-cascader :options="parentCateList" v-model="parentCate" expand-trigger="hover" :props="cascaderProps" @change="handleChange" clearable></el-cascader>
         </el-col>
       </el-row>
     </div>
     <div class="main" style="margin-bottom: 20px;">
       <el-alert title="注意：指允许为第三级分类设置相关参数！" type="warning" show-icon style="margin-bottom: 20px;" :closable="false"></el-alert>
-      <el-tabs v-model="activeName" @tab-click="handleClick">
-        <el-tab-pane label="动态参数" name="first">动态参数</el-tab-pane>
-        <el-tab-pane label="静态属性" name="second">静态属性</el-tab-pane>
+      <el-tabs v-model="activeName" @tab-click="handleTabClick">
+        <el-tab-pane label="动态参数" name="many">
+          <el-button type="primary" :disabled="isTrue" style="margin-bottom: 20px;" @click="AddParams">添加参数</el-button>
+          <el-table :data="activeList" border stripe>
+            <el-table-column type="expand" align="center" header-align="center"></el-table-column>
+            <el-table-column prop="attr_name" label="参数名称" align="center" header-align="center"></el-table-column>
+            <el-table-column label="操作" align="center" header-align="center">
+              <template slot-scope="scope">
+                <el-button @click="edit(scope.row)" type="primary" icon="el-icon-edit" size="mini">编辑</el-button>
+                <el-button @click="del(scope.row)" type="danger" icon="el-icon-delete" size="mini">删除</el-button>
+              </template>
+            </el-table-column>
+          </el-table>
+        </el-tab-pane>
+        <el-tab-pane label="静态属性" name="only">
+          <el-button type="primary" :disabled="isTrue" style="margin-bottom: 20px;" @click="AddParams">添加属性</el-button>
+          <el-table :data="staticList" border stripe>
+            <el-table-column type="expand" align="center" header-align="center"></el-table-column>
+            <el-table-column prop="attr_name" label="参数名称" align="center" header-align="center"></el-table-column>
+            <el-table-column label="操作" align="center" header-align="center">
+              <template slot-scope="scope">
+                <el-button @click="edit(scope.row)" type="primary" icon="el-icon-edit" size="mini">编辑</el-button>
+                <el-button @click="del(scope.row)" type="danger" icon="el-icon-delete" size="mini">删除</el-button>
+              </template>
+            </el-table-column>
+          </el-table>
+        </el-tab-pane>
       </el-tabs>
-      <add-cate v-if="dialogFormVisible" :visible.sync="dialogFormVisible" @closeDL="handleCloseDialog" @refreshList="onRefresList"></add-cate>
-      <edit-cate v-if="dialogFormVisible2" :visible.sync="dialogFormVisible2" @closeDL="handleCloseDialog2" :data="editInfos" @refreshList="onRefresList"></edit-cate>
+      <add-params v-if="dialogFormVisible" :visible.sync="dialogFormVisible" @closeDL="handleCloseDialog" @refreshList="onRefresList" :title="titleName" :parentCate="parentCate"></add-params>
+      <edit-params v-if="dialogFormVisible2" :visible.sync="dialogFormVisible2" @closeDL="handleCloseDialog2" :data="editInfos" @refreshList="onRefresList"></edit-params>
     </div>
   </div>
 </template>
@@ -29,10 +53,15 @@ export default {
       tableData: [],
       loading: false,
       parentCateList: [],
-      activeName: 'first',
+      parentCate: [],
+      activeName: 'many',
+      activeList: [],
+      staticList: [],
       dialogFormVisible: false,
       dialogFormVisible2: false,
       editInfos: {},
+      isTrue: true,
+      titleName: '',
       cascaderProps: {
         value: 'cat_id',
         label: 'cat_name',
@@ -41,8 +70,8 @@ export default {
     }
   },
   components: {
-    'add-cate': () => import('@/components/goods/cate/AddCate.vue'),
-    'edit-cate': () => import('@/components/goods/cate/EditCate.vue')
+    'add-params': () => import('@/components/goods/params/AddParams.vue'),
+    'edit-params': () => import('@/components/goods/params/EditParams.vue')
   },
   computed: {
     tableHeight() {
@@ -60,29 +89,6 @@ export default {
     init() {
       const _self = this
       _self.loading = true
-      // 列表接口
-      // _self
-      //   .$axios({
-      //     method: 'get',
-      //     url: _self.stateApi.sys.goods.cate.list,
-      //     params: {
-      //       pagenum: _self.pageInfos.pageindex,
-      //       pagesize: _self.pageInfos.pagesize
-      //     }
-      //   })
-      //   .then(response => {
-      //     const { data } = response
-      //     _self.loading = false
-      //     if (data.meta.status === 200) {
-      //       _self.tableData = data.data.result
-      //       _self.pageInfos.total = data.data.total
-      //     } else {
-      //       _self.$message.error(data.meta.msg)
-      //     }
-      //   })
-      //   .catch(() => {
-      //     _self.loading = false
-      //   })
       _self
         .$axios({
           method: 'get',
@@ -102,7 +108,7 @@ export default {
         })
     },
     /** 添加分类 */
-    addCategories() {
+    AddParams() {
       const _self = this
       _self.dialogFormVisible = true
     },
@@ -151,7 +157,30 @@ export default {
     /**
      * 父级分类
      */
-    handleChange (value) {},
+    handleChange(value) {
+      const _self = this
+      if (value.length === 3) {
+        _self.isTrue = false
+        _self.parentCate = value[value.length - 1]
+        _self.getList('many', _self.parentCate)
+        _self.titleName = '动态参数'
+      } else {
+        _self.isTrue = true
+      }
+    },
+    /**
+     * tabs
+     *  */
+    handleTabClick(tab, event) {
+      const _self = this
+      _self.getList(tab.name, _self.parentCate)
+      if (tab.name === 'many') {
+        _self.titleName = '动态参数'
+      } else {
+        _self.titleName = '静态属性'
+      }
+      
+    },
     /**
      * 在子页面刷新父页面数据 - 添加
      * @returns void
@@ -176,6 +205,42 @@ export default {
     handleCloseDialog2() {
       const _self = this
       _self.dialogFormVisible2 = false
+    },
+    /**
+     * 获取列表数据
+     */
+    getList(val, value) {
+      const _self = this
+      // 列表接口
+      _self
+        .$axios({
+          method: 'get',
+          url:
+            _self.stateApi.sys.goods.cate.list +
+            '/' +
+            value +
+            '/' +
+            'attributes',
+          params: {
+            sel: val
+          }
+        })
+        .then(response => {
+          const { data } = response
+          _self.loading = false
+          if (data.meta.status === 200) {
+            if (val === 'many') {
+              _self.activeList = data.data
+            } else {
+              _self.staticList = data.data
+            }
+          } else {
+            _self.$message.error(data.meta.msg)
+          }
+        })
+        .catch(() => {
+          _self.loading = false
+        })
     }
   },
   created() {
---
+++ shop/src/components/goods/params/AddParams.vue
@@ -0,0 +1,121 @@
+<template>
+  <el-dialog :title="'添加' + title" :visible="visible" width="38%" :before-close="closeDialog" @close="closeDialog" :close-on-click-modal="false">
+    <el-form :model="formInfo" :rules="rules" ref="formInfo">
+      <el-form-item :label="title" :label-width="formLabelWidth" prop="activeParams">
+        <el-input v-model="formInfo.activeParams" class="select"></el-input>
+      </el-form-item>
+    </el-form>
+    <span slot="footer" class="dialog-footer">
+      <el-button @click="handleCancle('formInfo')">取消</el-button>
+      <el-button type="primary" @click="handleSubmit('formInfo')" :disabled="loading">确定</el-button>
+    </span>
+  </el-dialog>
+</template>
+
+<script>
+import { mapState } from 'vuex'
+export default {
+  name: 'AddParams',
+  props: {
+    visible: {
+      required: true,
+      type: Boolean
+    },
+    title: {
+      type: String
+    },
+    parentCate: {
+      type: Number
+    }
+  },
+  data () {
+    const _self = this
+    return {
+      formLabelWidth: '180px',
+      formInfo: {
+        activeParams: ''
+      },
+      message: '',
+      loading: false,
+      rules: {
+        activeParams: [
+          {
+            required: true,
+            message: '请输入' + _self.title,
+            trigger: ['change', 'blur']
+          }
+        ]
+      }
+    }
+  },
+  computed: {
+    ...mapState({
+      stateApi: state => state.api
+    })
+  },
+  methods: {
+    /**
+     * 关闭弹框事件
+     * @returns void
+     */
+    closeDialog () {
+      const _self = this
+      _self.$emit('closeDL')
+    },
+    /**
+     * 取消按钮
+     * @returns void
+     */
+    handleCancle (formName) {
+      const _self = this
+      _self.closeDialog()
+      _self.$refs[formName].resetFields()
+    },
+    /**
+     * 确定发货按钮
+     * @returns void
+     */
+    handleSubmit (formName) {
+      const _self = this
+      _self.$refs[formName].validate(valid => {
+        if (valid) {
+          if (!_self.loading) {
+            _self.loading = true
+            _self
+              .$axios({
+                method: 'post',
+                url: _self.stateApi.sys.goods.cate.list +'/' + _self.parentCate +'/' +'attributes',
+                data: {
+                  attr_name: '',
+                  attr_sel: '',
+                  attr_vals: ''
+                }
+              })
+              .then(response => {
+                _self.loading = false
+                const { data } = response
+                if (data.meta.status === 201) {
+                  _self.$message.success(data.meta.msg)
+                  _self.closeDialog()
+                  _self.$emit('refreshList')
+                } else {
+                  _self.$message.error(data.meta.msg)
+                }
+              })
+              .catch(() => {
+                _self.loading = false
+              })
+          }
+        } else {
+          return false
+        }
+      })
+    }
+  }
+}
+</script>
+<style lang="less" scoped>
+.select {
+  width: 64%;
+}
+</style>
